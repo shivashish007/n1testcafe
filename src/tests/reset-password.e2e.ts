2e19d0510defce0465949889c24bdfb5b7fcce5e
shivashish007 <89742827+shivashish007@users.noreply.github.com>
2021-08-30 00:39:21 +0530

    Update reset-password.e2e.ts

--- src/tests/reset-password.e2e.ts
+++ src/tests/reset-password.e2e.ts
@@ -12,4 +12,22 @@ test
 	// 2. Get the code from the email (at n1 we use mailosaur.com, you have a free 15 days trial if you want to use this, or feel free to use any other method)
 	// 3. The code is used in reset-password.model.ts to reset the users model.
 	// 4. assert the correctness of the process.
+	export class ForgotPasswordService{
+	async forgotPassword(agrawal_shivashish@yahoo.com: string): Promise<boolean>{
+	const user= await User.findOne({email:agrawal_shivashish@yahoo.com});
+		if(!user){
+		return false;
+		}
+	const token= generateResetToken(user);
+	const link= 'https://app-demo.novemberfirst.com/#/public/enter-your-details'
+	await this.emailService.sendEmail({
+		from: 'shivashish007@gmail.com',
+		to: agrawal_shivashish@yahoo.com,
+		subject:'Reset Password',
+		text: 'URL to reset the Password: ${link}',
+		html:'<a href="${link}"> Click Here</a> to Reset Password',
+	});
+		return true;
+	}
+	}
 }).skip;
