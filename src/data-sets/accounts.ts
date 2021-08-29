43895fb6f687c58ed2afbf749317a84d6eff4b58
shivashish007 <89742827+shivashish007@users.noreply.github.com>
2021-08-29 22:30:08 +0530

    Update accounts.ts
    Please let me know the account Number format.
    Its not allowing to enter alphanumeric value.

--- src/data-sets/accounts.ts
+++ src/data-sets/accounts.ts
@@ -3,8 +3,8 @@ import { Account } from '../model/account';
 export const e2eaccount: Account = {
 	// TODO - store here the account number, email and password of the created account on demo environment.
 	accountNumber: '',
-	email: '',
-	password: '',
+	email: 'agrawal_shivashish@yahoo.com',
+	password: 'testcafe321',
 	environment: 'demo',
 	economicToken: ''
 };
